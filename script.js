6a8cc3647a730e69ccc132bca9d430bfe229cbf6
jashwantjaladi <jaladivenkatasai10001@skiff.com>
2023-10-03 16:38:21 +0530

    add grid and reset button

---
+++ script.js
@@ -0,0 +1,39 @@
+asksize=prompt("enter a number")
+const gridsize = document.querySelector(".gridsize");
+let gridlayout;
+if(asksize>100)
+{
+    gridsize.textContent = "Sorry no layout can be generated!"
+}else
+{
+    gridlayout = asksize*asksize;
+    gridsize.textContent= "the size of your grid is "+(asksize*asksize);
+}
+const container = document.querySelector(".container")
+for(let i=1;i<=gridlayout;i++)
+{
+    const div = document.createElement("div")
+    div.classList.add("containeritem")
+    container.appendChild(div)
+}
+
+let containeritem =document.querySelectorAll(".containeritem") 
+containeritem.forEach((item) =>
+{
+    item.addEventListener("mouseover", function(e)
+    {
+        item.style.cssText ="background-color : yellow"
+    })
+}
+
+)
+
+let reset = document.querySelector(".reset")
+reset.addEventListener("click", function(e)
+{
+    containeritem.forEach((item) =>
+    {
+        item.style.cssText="background-color : white"
+    })
+    
+})
